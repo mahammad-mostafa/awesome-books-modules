81a8da098834ade546340b1082b669c295319527
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-11 02:09:54 +0300

    Included id creation in constructor

--- modules/book.js
+++ modules/book.js
@@ -1,5 +1,6 @@
 export default class Book {
-  constructor(title, author) {
+  constructor(id, title, author) {
+    this.id = `${Date.now()}`;
     this.title = title;
     this.author = author;
   }
